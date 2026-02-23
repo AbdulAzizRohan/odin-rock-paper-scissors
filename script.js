7e2387d9039b7ebb9d4ec9516f0543fc35e75ce3
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2026-02-23 21:23:25 +0600

    Finished playRound function
    playRound function now can take userChoice and show result of the round.

--- script.js
+++ script.js
@@ -4,11 +4,46 @@
 
 function getComputerChoice() {
   let randomVal = Math.floor(Math.random() * 3) + 1;
-  if (randomVal === 1) return "Rock";
-  else if (randomVal === 2) return "Paper";
-  else return "Scissor";
+  if (randomVal === 1) return "rock";
+  else if (randomVal === 2) return "paper";
+  else return "scissor";
 }
 
 function getHumanChoice() {
   return prompt("What is your move (Rock, Paper or Scissor)?");
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+let didHumanWon = false;
+let isDraw = false;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock") {
+    if (computerChoice === "paper") didHumanWon = false;
+    else if (computerChoice === "scissor") didHumanWon = true;
+    else isDraw = true;
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "rock") didHumanWon = true;
+    else if (computerChoice === "scissor") didHumanWon = false;
+    else isDraw = true;
+  } else if (humanChoice === "scissor") {
+    if (computerChoice === "paper") didHumanWon = true;
+    else if (computerChoice === "rock") didHumanWon = false;
+    else isDraw = true;
+  }
+
+  if (isDraw) {
+    console.log(`It's a draw! Both of you played ${humanChoice}`);
+  } else if (didHumanWon) {
+    console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+  }
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
