fe227a916d6697b29c211518a12f5e177c9a8d3a
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2026-02-23 14:49:28 +0600

    Finished getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+// logic to get the computer choice
+// use Math.random() to get random numbers between 1 to 3
+// use the numbers to get computer choice
+
+function getComputerChoice() {
+  let randomVal = Math.floor(Math.random() * 3) + 1;
+  if (randomVal === 1) return "Rock";
+  else if (randomVal === 2) return "Paper";
+  else return "Scissor";
+}
