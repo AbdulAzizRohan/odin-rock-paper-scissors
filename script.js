c73deb70b02a002391d775046cc315d0a6134426
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2026-03-14 00:47:26 +0600

    Added initial UI and functional game logic
    Changed the game logic to declare a winner after winning five rounds.
    Added initial UI to play the using buttons only and display results.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// logic to get the computer choice
-// use Math.random() to get random numbers between 1 to 3
-// use the numbers to get computer choice
-
 function getComputerChoice() {
   let randomVal = Math.floor(Math.random() * 3) + 1;
   if (randomVal === 1) return "rock";
@@ -13,16 +9,23 @@ function getHumanChoice() {
   return prompt("What is your move (Rock, Paper or Scissor)?");
 }
 
-// main function for the game
-
 function playGame() {
-  // initialize the score and booleans
-
   let humanScore = 0;
   let computerScore = 0;
 
   let didHumanWon = false;
   let isDraw = false;
+
+  const roundResult = document.querySelector(".round-result");
+  const resultPara = document.createElement("p");
+  roundResult.style.display = "none";
+  roundResult.appendChild(resultPara);
+
+  const gameResult = document.querySelector(".game-result");
+  const gameResultPara = document.querySelector(".game-result-para");
+  const gameResultBtn = document.querySelector("#game-result-btn");
+  gameResult.style.display = "none";
+  gameResult.appendChild(gameResultPara);
 
   function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock") {
@@ -39,42 +42,68 @@ function playGame() {
       else isDraw = true;
     }
 
-    // determine the round winner and update their score
+    const humanScoreUpdate = document.querySelector(".human");
+    const computerScoreUpdate = document.querySelector(".computer");
 
     if (isDraw) {
-      console.log(`It's a draw! Both of you played ${humanChoice}`);
+      resultPara.textContent = `It's a draw! Both of you played ${humanChoice}`;
+      roundResult.style.display = "block";
     } else if (didHumanWon) {
-      console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+      resultPara.textContent = `You won! ${humanChoice} beats ${computerChoice}.`;
+      roundResult.style.display = "block";
+
       humanScore++;
+      humanScoreUpdate.textContent = `You: ${humanScore}`;
     } else {
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      resultPara.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
+      roundResult.style.display = "block";
+
       computerScore++;
+      computerScoreUpdate.textContent = `Computer: ${computerScore}`;
     }
 
-    // reset the booleans for the next round
+    if (humanScore === 5) {
+      gameResultPara.textContent = "Wow! You won!";
+      gameResult.style.display = "block";
+
+      gameResultBtn.addEventListener("click", resetGame);
+    } else if (computerScore === 5) {
+      gameResultPara.textContent = "You lose!";
+      gameResult.style.display = "block";
+
+      gameResultBtn.addEventListener("click", resetGame);
+    }
 
     didHumanWon = false;
     isDraw = false;
+
+    function resetGame() {
+      computerScore = 0;
+      humanScore = 0;
+
+      humanScoreUpdate.textContent = `You: ${humanScore}`;
+      computerScoreUpdate.textContent = `Computer: ${computerScore}`;
+
+      roundResult.style.display = "none";
+      gameResult.style.display = "none";
+    }
   }
 
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice().toLowerCase(), getComputerChoice());
-  }
+  const rock = document.querySelector("#rock");
+  const paper = document.querySelector("#paper");
+  const scissor = document.querySelector("#scissor");
 
-  // determine the winner of the game
+  rock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+  });
 
-  if (humanScore === computerScore) {
-    console.log("It's a draw!");
-  } else if (humanScore > computerScore) {
-    console.log("Wow! You won!");
-  } else {
-    console.log("You lose!");
-  }
+  paper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+  });
 
-  // reset the score for the next game
-
-  humanScore = 0;
-  computerScore = 0;
+  scissor.addEventListener("click", () => {
+    playRound("scissor", getComputerChoice());
+  });
 }
 
 playGame();
